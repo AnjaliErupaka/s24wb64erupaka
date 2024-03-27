fb0a314f024de152ebf10e70fc3bf40257f29604
AnjaliErupaka <anjalisrinivas174@gmail.com>
2024-03-27 16:07:37 -0500

    adding images

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var dogRouter = require('./routes/dog'); // Import the dogRouter
 var dogRouter = require('./routes/dogs');
 var gridRouter = require('./routes/grid');
-// var pickRouter = require('./routes/pick');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -27,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dogs', dogRouter); 
 app.use('/grid', gridRouter);
-// app.use('/pick', pickRouter);
+app.use('/pick', pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
